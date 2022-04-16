5d2f8f4c09b6305c4e4b898791250c67d20057de
Daniel Sousa <daniel.sousa.developer@gmail.com>
2022-04-16 09:41:04 -0300

    feat: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-valdiation.spec.ts
+++ src/validation/required-field/required-field-valdiation.spec.ts
@@ -1,4 +1,5 @@
 import {RequiredFieldError} from '@/validation/errors/required-field-error';
+import faker from '@faker-js/faker';
 import {RequiredFieldValidation} from './required-field-validation';
 
 describe('RequiredFieldValdiation', () => {
@@ -6,5 +7,11 @@ describe('RequiredFieldValdiation', () => {
     const sut = new RequiredFieldValidation('email');
     const error = sut.validate('');
     expect(error).toEqual(new RequiredFieldError());
+  });
+
+  test('should return falsy if filed is not empty', () => {
+    const sut = new RequiredFieldValidation('email');
+    const error = sut.validate(faker.random.words());
+    expect(error).toBeFalsy();
   });
 });
