6ef084bdc405e1bfbd6cdeb8588a847c785f1f8f
Daniel Sousa <daniel.sousa.developer@gmail.com>
2022-04-16 11:15:46 -0300

    fix: ensure EmailValidation allows empty values

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -8,13 +8,19 @@ const makeSut = (): EmailValidation =>
 describe('EmailValidation', () => {
   test('should return error if email is invalid', () => {
     const sut = makeSut();
-    const error = sut.validate('');
+    const error = sut.validate(faker.random.words());
     expect(error).toEqual(new InvalidFieldError());
   });
 
   test('should return falsy if email is valid', () => {
     const sut = makeSut();
     const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
+  });
+
+  test('should return falsy if email is empty', () => {
+    const sut = makeSut();
+    const error = sut.validate('');
     expect(error).toBeFalsy();
   });
 });
--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -6,6 +6,6 @@ export class EmailValidation implements FieldValidation {
   validate(value: string): Error | null {
     const emailRegex =
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return emailRegex.test(value) ? null : new InvalidFieldError();
+    return !value || emailRegex.test(value) ? null : new InvalidFieldError();
   }
 }
