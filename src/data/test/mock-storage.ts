06cce5a97f873e7ffc047cce7dfdb62a3ab5badf
Daniel Sousa <daniel.sousa.developer@gmail.com>
2022-04-21 08:07:45 -0300

    refactor: create LocalSaveStorage make Sut

--- src/data/test/mock-storage.ts
+++ src/data/test/mock-storage.ts
@@ -1,6 +1,6 @@
 import {SetStorage} from '../protocols/cache/set-storage';
 
-export class SetStorageSpy implements SetStorage {
+export class SetStorageMock implements SetStorage {
   key: string | undefined;
   value: any;
   set(key: string, value: any): Promise<void> {
--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,11 +1,25 @@
-import {SetStorageSpy} from '@/data/test/mock-storage';
+import {SetStorageMock} from '@/data/test/mock-storage';
 import faker from '@faker-js/faker';
 import {LocalSaveAccessToken} from './local-save-access-token';
 
+type SutTypes = {
+  sut: LocalSaveAccessToken;
+  setStorage: SetStorageMock;
+};
+
+const makeSut = (): SutTypes => {
+  const setStorage = new SetStorageMock();
+  const sut = new LocalSaveAccessToken(setStorage);
+
+  return {
+    sut,
+    setStorage,
+  };
+};
+
 describe('LocalSaveAccessToken', () => {
   test('should call set Storage with correct value', () => {
-    const setStorage = new SetStorageSpy();
-    const sut = new LocalSaveAccessToken(setStorage);
+    const {sut, setStorage} = makeSut();
     const accessToken = faker.random.words();
     sut.save(accessToken);
     expect(setStorage.key).toBe('accessToken');
