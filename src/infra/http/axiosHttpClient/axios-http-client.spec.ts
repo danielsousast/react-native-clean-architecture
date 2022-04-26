9d7d415624df62b37e05177c92ea7f0eb20ec266
Daniel Sousa <daniel.sousa.developer@gmail.com>
2022-04-26 14:35:03 -0300

    feat: emnsure AxiosHttpClient calls axios.post with correct values

--- src/infra/http/axiosHttpClient/axios-http-client.spec.ts
+++ src/infra/http/axiosHttpClient/axios-http-client.spec.ts
@@ -26,10 +26,32 @@ describe('AxiosHttpClient', () => {
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
   });
 
+  test('should return correct response on axios.post', async () => {
+    const request = mockPostRequest();
+    const {sut, mockedAxios} = makeSut();
+    const httpResponse = await sut.post(request);
+    const axiosResponse = await mockedAxios.post.mock.results[0]?.value;
+    expect(httpResponse).toEqual({
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    });
+  });
+
   test('should call axios.get with correct values', async () => {
     const request = mockGetRequest();
     const {sut, mockedAxios} = makeSut();
     sut.get(request);
     expect(mockedAxios.get).toHaveBeenCalledWith(request.url);
+  });
+
+  test('should return correct response on axios.get', async () => {
+    const request = mockGetRequest();
+    const {sut, mockedAxios} = makeSut();
+    const httpResponse = await sut.get(request);
+    const axiosResponse = await mockedAxios.get.mock.results[0].value;
+    expect(httpResponse).toEqual({
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    });
   });
 });
