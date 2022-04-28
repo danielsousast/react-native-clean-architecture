fa6f7e9c6bd9eff4ac436b7aabcdee4aaf02fae4
Daniel Sousa <daniel.sousa.developer@gmail.com>
2022-04-28 14:01:00 -0300

    test: ensure CurrentAccountAdapter throws if an invalid account is provided

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,3 +1,4 @@
+import {UnexpectedError} from '@/domain/errors';
 import {mockAccountModel} from '@/domain/test';
 import {LocalStorageAdapter} from '@/infra/cache/local-storage-adapter';
 import {setCurrentAccountAdapter} from './current-account-adapter';
@@ -8,5 +9,11 @@ describe('CurrentAccountAdapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set');
     setCurrentAccountAdapter(account);
     expect(setSpy).toHaveBeenCalledWith('account', JSON.stringify(account));
+  });
+
+  test('should throw UnexpectedError', () => {
+    expect(() => {
+      setCurrentAccountAdapter(undefined as any);
+    }).toThrow(new UnexpectedError());
   });
 });
--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -3,7 +3,7 @@ import {AccountModel} from '@/domain/models';
 import {makeLocalStorageAdapter} from '@/main/factories/usecases';
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-  if (!account.accessToken) {
+  if (!account?.accessToken) {
     throw new UnexpectedError();
   }
   makeLocalStorageAdapter().set('account', JSON.stringify(account));
