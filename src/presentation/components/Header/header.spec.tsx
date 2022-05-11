0d57c5d9a22f1ba12e0dad1c9971f04e0a75ad67
Daniel <daniel.sousa.developer@gmail.com>
2022-05-11 15:38:00 -0300

    refactor: move header sut to makeSut factory

--- src/presentation/components/Header/header.spec.tsx
+++ src/presentation/components/Header/header.spec.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { renderWithAuthProvider } from "@/../jest/helpers"
 import Header from "@/presentation/components/Header"
-import { act, fireEvent } from "@testing-library/react-native";
+import { act, fireEvent, RenderAPI } from "@testing-library/react-native";
+import { AccountModel } from '@/domain/models';
+
+type SutTypes = {
+    sut: RenderAPI;
+    setCurrentAccountMock: (account: AccountModel) => void;
+}
+
+const makeSut = ():SutTypes => {
+    const setCurrentAccountMock = jest.fn();
+    const sut = renderWithAuthProvider(
+        {
+            component: <Header title="any"/>, 
+            setCurrentAccount: setCurrentAccountMock
+        }
+    )
+
+    return {
+        sut, setCurrentAccountMock
+    }
+}
 
 describe("Header component", () => {
     test("should call setCurrentAccount with null value", () => {
-        const setCurrentAccountMock = jest.fn();
-        const {getByTestId} = renderWithAuthProvider(
-            {
-                component: <Header title="any"/>, 
-                setCurrentAccount: setCurrentAccountMock
-            }
-        )
-
-        const signOutButton = getByTestId('signout-button');
+        const {sut,setCurrentAccountMock} = makeSut();
+            const signOutButton = sut.getByTestId('signout-button');
         act(() => {
             fireEvent.press(signOutButton);
         });
